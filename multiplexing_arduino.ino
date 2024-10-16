16392b97d96558d91b7139de60fd2115b7300d37
Pavlos <79235112+pavlosconstas@users.noreply.github.com>
2024-10-16 18:28:53 -0400

    Update multiplexing_arduino.ino

--- multiplexing_arduino.ino
+++ multiplexing_arduino.ino
@@ -4,22 +4,22 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-const float freq1 = 1.0; 
-const float freq2 = 0.5;
-const float freq3 = 2.0; 
-const float freq4 = 1.5; 
+const float freq1 = 400; 
+const float freq2 = 10;
+const float freq3 = 120; 
+const float freq4 = 60; 
 
 const int pwmMax = 4095; 
 
 void setup() {
-  Serial.begin(9600);
+  Wire.setClock(400000);
   pwm.begin();
-  pwm.setPWMFreq(1600);
+  pwm.setPWMFreq(1526);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  float t = currentMillis / 1000.0; 
+  unsigned long currentMicros = micros();
+  float t = currentMicros / 1000000.0;
 
   int brightness1 = (pwmMax / 2) + (pwmMax / 2) * sin(2 * PI * freq1 * t);  // LED 1 (PWM0)
   int brightness2 = (pwmMax / 2) + (pwmMax / 2) * sin(2 * PI * freq2 * t);  // LED 2 (PWM1)
@@ -30,14 +30,4 @@ void loop() {
   pwm.setPWM(1, 0, brightness2);
   pwm.setPWM(2, 0, brightness3); 
   pwm.setPWM(3, 0, brightness4);
-
-  Serial.print(brightness1);
-  Serial.print(",");          
-  Serial.print(brightness2); 
-  Serial.print(",");         
-  Serial.print(brightness3); 
-  Serial.print(",");          
-  Serial.println(brightness4);
-
-  delay(10);
 }
